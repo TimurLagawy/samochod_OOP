171a19df2530a561e0202bd02fd4c9cd11a10b79
Timur Lagawy <timurtagirovich@mail.ru>
2023-02-09 11:53:40 +0100

    Transport_packages_static

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ import db.DB;
 
 public class Main {
     public static void main (String[] args) {
+        final int count = 10;
+        Person obj1 = new Person();
+        Person.getCount();
+        Person obj2 = new Person();
+        Person.getCount();
+        Person obj3 = new Person();
+        Person obj4 = new Person();
+        Person.getCount();
+
+//        Person.count = 5;
+//        System.out.println(Person.count);
         DB db = new DB();
         info("Hello world ", 4.0f);
     }
